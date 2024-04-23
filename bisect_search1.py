c36af1317210827b157d085f1bd23c8ae3c8db07
xeroxzen <andilembele020@gmail.com>
2024-04-23 18:01:33 +0200

    Refactor bisect_search1 function in bisect_search1.py

--- bisect_search1.py
+++ bisect_search1.py
@@ -5,18 +5,18 @@ Created on Mon Jul 26 17:23:16 2021
 """
 
 
-def bisect_search1(L, e):
-    """where L is list and e is element"""
-    if L == []:
+def bisect_search1(data_list, e):
+    """where data_list is list and e is element"""
+    if data_list == []:
         return False
-    elif len(L) == 1:
+    elif len(data_list) == 1:
         return L[0] == e
     else:
-        half = len(L) // 2
-        if L(half) > e:
-            return bisect_search1(L[:half], e)
+        half = len(data_list) // 2
+        if data_list(half) > e:
+            return bisect_search1(data_list[:half], e)
         else:
-            return bisect_search1(L[half:], e)
+            return bisect_search1(data_list[half:], e)
 
 
 test_list = [1, 2, 3, 5, 7, 9, 18, 27]
